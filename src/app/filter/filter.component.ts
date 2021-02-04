26b7c3796f6523766073aa304b90a6fa770475a2
Yan <krip2n@gmail.com>
2021-02-04 21:56:32 +0300

    add select box with hardcoded genres

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Genre } from '../interfaces/Genre';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrls: ['./filter.component.scss'],
 })
 export class FilterComponent implements OnInit {
-
+  genres: Genre[] = [
+    { id: 1, title: 'Techno' },
+    { id: 2, title: 'Chillout' },
+    { id: 3, title: 'Progressive' },
+  ];
+  
   tracksFilter = this.formBuilder.group({
-    category: ''
+    genre: [this.genres[0]]
   });
-  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
 
   onSubmit(): void {
     console.log('onSubmit');
   }
-
 }
